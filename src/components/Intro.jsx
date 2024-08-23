75a2c1c19c7460b4918f419861e930201f32bdd7
alaguelo <alaguelo@ucsd.edu>
2024-08-23 16:17:57 -0700

    initial commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20
+        pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Altair Aguelo</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Aspiring Software Engineer & 3rd Year Computer Science Major at UCSD</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">
+                Aspiring software developer with proficiency in coding and 
+                experience in computer science outreach. Driven by a passion for 
+                computer science, I am committed to enhancing my skills and knowledge 
+                through hands-on projects and real-world experience, striving to excel 
+                and make meaningful contributions in the field, as well as make 
+                technology more accessible through the things I program. 
+                <br></br>
+                Check out some of my work on my {' '}
+                <a
+                    href="https://github.com/altairaguelo"
+                    target = "_blank"
+                    className="text-cyan-600 hover:underline underline-offset-2 decoration-2
+                    decoration-blue-600"
+                    rel="noreferrer noopener"
+                >
+                    Github
+                </a>
+                !
+            </p>
+        </div>
+    )
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,37 @@
+import devlogsc from '../assets/devlogsc.png';
+import travelsc from '../assets/sdhacksc.png';
+import sentimentsc from '../assets/sentimentsc.png';
+import urlsc from '../assets/5footsc.png';
+import dogsc from '../assets/dogwebsc.png';
+export default [
+    {
+        title: 'Developer Log',
+        imgUrl: devlogsc,
+        stack: ['HTML', 'CSS', 'JavaScript', 'Puppeteer'],
+        link: 'https://cse110-sp24-group10.github.io/cse110-sp24-group10/source/calendar/calendar.html',
+    },
+    {
+        title: 'Travel Planner',
+        imgUrl: travelsc,
+        stack: ['NextJS', 'MongoDB', 'GoogleMaps API'],
+        link: 'https://github.com/altairaguelo/sp24-hack-team-1',
+    },
+    {
+        title: 'Sentiment Widget',
+        imgUrl: sentimentsc,
+        stack: ['HTML', 'CSS', 'Java'],
+        link: 'https://cse110-sp24-group10.github.io/warmup-exercise/',
+    },
+    {
+        title: 'URL Shortener',
+        imgUrl: urlsc,
+        stack: ['React', 'Python', 'Firebase', 'Render'],
+        link: 'https://fivefooturl.onrender.com',
+    },
+    {
+       title: 'Dog Showcase Website',
+       imgUrl: dogsc,
+       stack: ['HTML', 'CSS'],
+       link: 'https://alaguelo.github.io/dog-website/',
+    },
+]
