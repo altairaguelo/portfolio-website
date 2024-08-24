2142f6390ae16bb1cf9d458a15bcb2603aa8102e
alaguelo <alaguelo@ucsd.edu>
2024-08-23 18:08:24 -0700

    added homepage url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/altairaguelo/portfolio-website',
+  base: '/portfolio-website/',
 })
