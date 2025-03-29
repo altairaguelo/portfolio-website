8d8f7c8b149c0fb29eda7c16cded5f923861e81a
altairaguelo <altairlance@gmail.com>
2025-03-29 00:01:41 +0000

    Build: (0615711) update 24/7 Teach

--- assets/index-C75Yy_mO.js
+++ assets/index-C75Yy_mO.js
@@ -53,12 +53,12 @@ Error generating stack: `+o.message+`
         I've done everything I could to learn how they work. Now, pursuing a bachelor's
         degree in Computer Science, I continue to explore the endless boundaries of 
         technology. Currently, I am passionate in UX/UI and front-end design, but would also 
-        like to explore data, AI, and game development.`},{year:"Today",title:"Web Design and Development Superstar",duration:"3 months",details:`24/7 Teach is an online education program geared towards education
-         through tech and experiential learning. Currently, I am a front end dev working 
-         in a team on an AI chatbot that helps users navigate our website and
-         find educational opportunities more efficiently.`},{year:"Today",title:"ACM Social Director",duration:"1 year",details:`ACM (Association for Computing Machinery) is UCSD's biggest pre-professional 
+        like to explore data, AI, and game development.`},{year:"Today",title:"ACM Social Director",duration:"1 year",details:`ACM (Association for Computing Machinery) is UCSD's biggest pre-professional 
         computing organization with over 2000 members. As a Social Director, I ensure that 
-        members have a safe and fun space to build connection and commmunity in a CS space.`},{year:"2021 - 2022",title:"Computer Science Outreach Advocate",duration:"2 Years",details:`As a member of UCSD Action Cycle, I advocated for computer science
+        members have a safe and fun space to build connection and commmunity in a CS space.`},{year:"2024 (June - December)",title:"Web Design and Development Superstar",duration:"6 months",details:`24/7 Teach is an online education program geared towards education
+         through tech and experiential learning. As a front end dev, I worked 
+         in a team to implement an AI/Live Support Chatbot using HTML/CSS/Javascript/ChatGPT 
+         API that assists users in exploring the company site to find personalized learning programs.`},{year:"2021 - 2022",title:"Computer Science Outreach Advocate",duration:"2 Years",details:`As a member of UCSD Action Cycle, I advocated for computer science
         opportunities in our low income area. Managing a team, we produced a website 
         through collaborative work, where our website reached over 100+ students`},{year:"2021 - 2022",title:"Computer Science Club Board",duration:"2 Years",details:`In Morse Codes Club, I oversaw event planning/CS Outreach. In a team
         we planned a CS Fair that attracted 200+ students. During that fair, I 
